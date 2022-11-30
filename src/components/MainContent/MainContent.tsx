f236df946d57c6d9342e9cf58a4adaa82de61c33
ibanb04 <ibanb040@gmail.com>
2022-11-30 01:48:33 -0500

    Creacion del MainContent

---
+++ src/components/MainContent/MainContent.tsx
@@ -0,0 +1,67 @@
+import OjingeoMuchim5 from "../../assets/Ojingeo-muchim-5.png";
+import OjingeoMuchim from "../../assets/Ojingeo-muchim.png";
+import OjingeoMuchim1 from "../../assets/Ojingeo-muchim-1.png";
+import cherry from "../../assets/cherry.png";
+import CardRecipe from '../CardRecipe/CardRecipe';
+import CardRecipeMobile from '../CardRecipe/CardRecipeMobile';
+const MainContent = () => {
+    const tarjets = [];
+    tarjets.push({
+        idnormal: "normal1",
+        idhover: "hover1",
+        imgPlate: OjingeoMuchim5,
+        food: "Ojingeo",
+        foodDesc: "Muchim",
+        calification: "5.0",
+    });
+    tarjets.push({
+        idnormal: "normal2",
+        idhover: "hover2",
+        imgPlate: OjingeoMuchim,
+        food: "Cola",
+        foodDesc: "Chicken",
+        calification: "5.0",
+    });
+    tarjets.push({
+        idnormal: "normal3",
+        idhover: "hover3",
+        imgPlate: OjingeoMuchim1,
+        food: "Roasted",
+        foodDesc: "Carrot",
+        calification: "4.5",
+    });
+    tarjets.push({
+        idnormal: "normal4",
+        idhover: "hover4",
+        imgPlate: cherry,
+        food: "Sweet",
+        foodDesc: "Cherries",
+        calification: "4.0",
+    });
+    return (
+        <>
+            <div className="middle">
+                <div className="middleTitle">Nuevas Recetas</div>
+                <div className="contCarrusel">
+
+                    <ul id="carruselini" className="carrusel">
+                        {
+                            tarjets.map((recipe) => (
+                                <CardRecipe {...recipe} />
+                            ))
+                        }
+                    </ul>
+                    <ul id="carruselfin" className="carrusel">
+                        {
+                            tarjets.map((recipe) => (
+                                <CardRecipeMobile {...recipe} />
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default MainContent
