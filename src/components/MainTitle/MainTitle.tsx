e92a5ce4e346555f008bc9484643869165fde803
ibanb04 <ibanb040@gmail.com>
2022-11-30 01:50:32 -0500

    Creacion del MainTitle

---
+++ src/components/MainTitle/MainTitle.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MainTitle = () => {
+    return (
+        <div className="headform">
+            <div className="imgHead">
+                <div className="floatText">
+                    <div className="text1">Recetas</div>
+                    <div className="text2">para todos</div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default MainTitle
