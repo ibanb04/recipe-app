dba9f00c60b12913842036723104b81459984236
ibanb04 <ibanb040@gmail.com>
2022-12-01 00:38:04 -0500

    Estilos para el Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import img_footer from '../../assets/Grupo-7610.png'
+import styles from './Footer.module.scss'
+
 const Footer = () => {
     return (
-        <div className="footer">
-            <div className="textfooter"> <span>Con el patrocinio de</span></div>
-            <img className="imgFooter" src={img_footer} />
+        <div className={styles.footer}>
+            <div className={styles.textfooter}> <span>Con el patrocinio de</span></div>
+            <img className={styles.imgFooter} src={img_footer} />
         </div>
     )
 }
