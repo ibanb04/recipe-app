49ab651094b06f77d8ee538c757889958888ad3d
ibanb04 <ibanb040@gmail.com>
2022-11-30 22:25:56 -0500

    Aplicación de estilos  sass  para el navbar y el mainTitle

--- src/components/MainTitle/MainTitle.tsx
+++ src/components/MainTitle/MainTitle.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import styles from './MainTitle.module.scss'
 const MainTitle = () => {
     return (
-        <div className="headform">
-            <div className="imgHead">
-                <div className="floatText">
-                    <div className="text1">Recetas</div>
-                    <div className="text2">para todos</div>
+        <div className={styles.headform}>
+            <div className={styles.imgHead}>
+                <div className={styles.floatText}>
+                    <div className={styles.text1}>Recetas</div>
+                    <div className={styles.text2}>para todos</div>
                 </div>
             </div>
         </div>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addCategory } from '../../store/slices/categories/categoriesSlice'
 import { RootState } from '../../store/store'
 import { navbarItems } from './utils/navbarItems'
-
+import styles from './NavBar.module.scss'
 
 
 const NavBar = () => {
@@ -14,23 +13,21 @@ const NavBar = () => {
     }
 
     return (
-        <div className="headform">
-            <div className="header">
-                <div className="title">
-                    <span className="text1">Recipe<span className="text2">App</span></span>
-                </div>
-                <div className="menuHead">
-                    <ul>
-                        {
-                            navbarItems.map((item, index) => (
-                                <li key={index} className={nameCategory === item.path ? "selected" : ""} onClick={() => handleCategory(item.path)}>{item.title}</li>
-                            ))
-                        }
-
-                    </ul>
-                </div>
-                <div className="icoHome"></div>
+        <div className={styles.header}>
+            <div className={styles.title}>
+                <span className={styles.text1}>Recipe<span className={styles.text2}>App</span></span>
             </div>
+            <div className={styles.menuHead}>
+                <ul>
+                    {
+                        navbarItems.map((item, index) => (
+                            <li key={index} className={nameCategory === item.path ? "selected" : ""} onClick={() => handleCategory(item.path)}>{item.title}</li>
+                        ))
+                    }
+
+                </ul>
+            </div>
+            <div className={styles.icoHome}></div>
         </div>
     )
 }
